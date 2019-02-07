3e2f389277eae34579a2eb401bfb9d0c1e73d277
Gerald Manipon <pymonger@gmail.com>
2019-02-07 23:05:26 +0000

    format to PEP8 spec

--- test/test_doc.py
+++ test/test_doc.py
@@ -61,7 +61,8 @@ def test_ProvEsDocument():
     out_accessURL = 'https://aria-search.domain.com/?source={"query":{"bool":{"must":[{"term":{"dataset":"interferogram"}},{"query_string":{"query":"\"interferogram__T111_F330-343_INSAR1_20140922-INSAR1_20140906\"","default_operator":"OR"}}]}},"sort":[{"_timestamp":{"order":"desc"}}],"fields":["_timestamp","_source"]}'
     out_downloadURL = 'https://dav.domain.com/repository/products/interferograms/v0.2/2014/09/06/interferogram__T111_F330-343_INSAR1_20140922-INSAR1_20140906/2014-09-22T224943.621648'
     out_level = "L1"
-    doc.dataset(out_id, out_doi, [out_downloadURL], [instrument], None, out_level)
+    doc.dataset(out_id, out_doi, [out_downloadURL], [
+                instrument], None, out_level)
 
     # software agent
     sa_id = "hysds:ariamh-worker-32.domain.com/12353"
@@ -81,7 +82,7 @@ def test_ProvEsDocument():
                          end_time.isoformat() + 'Z', [software],
                          sa_id, rt_ctx_id, [id, dem_id], [out_id],
                          wasAssociatedWithRole="softwareAgent")
-    
+
     print(doc.serialize(indent=2))
 
 
