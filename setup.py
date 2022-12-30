6c0f02f84a33b4730b8a4ade0c39b45472663e4b
dustinlo <dustin.k.lo@jpl.nasa.gov>
2022-12-29 17:12:19 -0800

    update setup.py bump version and added circleci tests

--- setup.py
+++ setup.py
@@ -6,10 +6,25 @@ from setuptools import setup, find_packages
 
 setup(
     name='prov_es',
-    version='0.2.2',
+    version='0.2.3',
     long_description='PROV-ES python library',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['prov==1.3.1', 'future>=0.17.1']
+    install_requires=['prov==1.3.1', 'future>=0.17.1'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
 )
